2e549f9b54b272bc01910b3de207f618f23e37fe
Alexander Karari <karari.alexander@gmail.com>
2025-02-10 16:36:49 +0300

    Add game_active flag.

--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,11 @@ class GameStats:
         self.ai_settings = ai_settings
         self.reset_stats()
 
+        # Start alien invasion in an inactive state.
+        self.game_active = True
+
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.ai_settings.ship_limit
+
+    
