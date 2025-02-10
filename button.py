89b34ada98daa25e07ddba1dd5adb7e16d59693e
Alexander Karari <karari.alexander@gmail.com>
2025-02-10 17:21:42 +0300

    rename centre to center.

--- button.py
+++ button.py
@@ -15,7 +15,7 @@ class Button:
 
         # Build the button's rect object and centre it
         self.rect = pygame.Rect(0, 0, self.width, self.height)
-        self.rect.centre = self.screen_rect.centre
+        self.rect.center = self.screen_rect.center
 
         # The button message needs to be prepped only once
         self.prep_msg(msg)
