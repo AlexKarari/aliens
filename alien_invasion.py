d4feea49ae71d488228a73bb77aeca5e49471dbe
Alexander Karari <karari.alexander@gmail.com>
2025-02-10 17:20:35 +0300

    Add play button to screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from pygame.sprite import Group
 from settings import Settings
 
 from game_stats import GameStats
+from button import Button
 
 from ship import Ship
 
@@ -20,6 +21,9 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height)
     )  # surface - display game element here
     pygame.display.set_caption("Alien Invasion")
+
+    # Make the Play button.
+    play_button = Button(ai_settings, screen, "Play")
 
     # Create an instance to store game statistics.
     stats = GameStats(ai_settings)
@@ -41,7 +45,7 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 
 run_game()
